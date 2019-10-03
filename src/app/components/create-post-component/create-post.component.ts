4557144afa373c88320948267d0d58d6b257ab11
Christian <43329131+CTKNguyen@users.noreply.github.com>
2019-10-03 12:50:40 +1000

    add image input validation

--- src/app/components/create-post-component/create-post.component.ts
+++ src/app/components/create-post-component/create-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
@@ -10,12 +11,22 @@ export class CreatePostComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
   imgURL: string | ArrayBuffer;
+  errorMessage: string;
  
   // Function source: https://www.talkingdotnet.com/show-image-preview-before-uploading-using-angular-7/
   seeImage(files) {
+    this.imgURL = "";
+    this.errorMessage = "";
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.errorMessage = "Only images can be uploaded";
+      return;
+    }
+
     var fileReader = new FileReader();
     fileReader.readAsDataURL(files[0]); 
     fileReader.onload = (_event) => { 
