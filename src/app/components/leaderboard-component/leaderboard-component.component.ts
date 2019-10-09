178fec380966f79c30562f25deb2aad827ee1527
jackson rajbhandari <jacksonrajbhandari14@gmail.com>
2019-10-09 13:21:22 +1100

    connecting leaderboard to the database

---
+++ src/app/components/leaderboard-component/leaderboard-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leaderboard-component',
+  templateUrl: './leaderboard-component.component.html',
+  styleUrls: ['./leaderboard-component.component.css']
+})
+export class LeaderboardComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
